2e7eea414e70869e7c50c1a5717022913ec7a21b
jyw <jyw2708@gmail.com>
2020-03-18 18:44:51 +0900

    server example

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hi");
+// requrie =: node moudels을 어딘가에 가져오는 경우
+const express = require("express");
+const app = express();
+
+const PORT = 1234;
+
+const handleListenting = () => {
+  console.log(`Listenting on: http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListenting);
