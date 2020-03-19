6b1761c4090bf13b05a850218830b40572d7cd1b
jyw <jyw2708@gmail.com>
2020-03-19 16:35:04 +0900

    with babel and nodemon

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 // requrie =: node moudels을 어딘가에 가져오는 경우
-const express = require("express");
+import "core-js";
+import express from "express";
 const app = express();
 
-const PORT = 1234;
+const PORT = 4000;
 
 const handleListenting = () => {
   console.log(`Listenting on: http://localhost:${PORT}`);
 };
+// 서버 기본 동작방식 ( nodejs with express )
+const handleHome = (req, res) => {
+  res.send("Hello from home 232");
+};
+
+const handleProfile = (req, res) => {
+  res.send("you are on my profile");
+};
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListenting);
